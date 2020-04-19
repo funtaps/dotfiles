98520049ef692ddc43a7389e8899030cfa9fab30
Кирилл Фунтов <funtovkd@eruditor-group.com>
2020-04-20 01:48:40 +0300

    setup awesome

--- .arch_stuff/setup/1-pacman.sh
+++ .arch_stuff/setup/1-pacman.sh
@@ -2,6 +2,6 @@ sudo pacman -Syu
 sudo pacman -S --needed base linux-zen linux-firmware
 sudo pacman -S --needed networkmanager
 sudo pacman -S --needed sudo reflector rsync
-sudo pacman -S --needed base-devel git encfs openssh
+sudo pacman -S --needed base-devel git encfs openssh neovim
 
 
