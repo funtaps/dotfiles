8c6f7098fc2831c5546b9687656b012e11af6f79
Кирилл Фунтов <funtovkd@eruditor-group.com>
2020-04-20 01:20:49 +0300

    setup 2

--- setup/1-pacman.sh
+++ setup/1-pacman.sh
@@ -1,6 +1,7 @@
 pacman -Syu
 pacman -S --needed base linux-zen linux-firmware
-pacman -S --needed base-devel git
-pacman -S --needed sudo reflector
+pacman -S --needed networkmanager
+pacman -S --needed sudo reflector rsync
+pacman -S --needed base-devel git encfs openssh
 
 
