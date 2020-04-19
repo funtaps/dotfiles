ab5e5f0a09904939d462fefc188c80e15f8cbd86
Кирилл Фунтов <funtovkd@eruditor-group.com>
2020-04-20 02:19:52 +0300

    setup terminal

--- .arch_stuff/setup/5-awesome-utils.sh
+++ .arch_stuff/setup/5-awesome-utils.sh
@@ -1,1 +1,5 @@
-sudo pacman -S --needed arandr network-manager-applet udiskie blueberry bluez-utils dex light-locker rofi
+sudo pacman -S --needed arandr network-manager-applet udiskie blueberry bluez-utils dex light-locker rofi flameshot picom playerctl xfce4-power-manager 
+sudo pacman -S  --needed chromium gnome-font-viewer gnome-keyring gparted gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb inotify-tools libgnome-keyring mate-system-monitor polkit-gnome seahorse smplayer smplayer-themes
+sudo pacman -S --needed thunar thunar-archive-plugin thunar-volman tumbler atool unrar xarchiver
+sudo pacman -S --needed pulseaudio pulseaudio-bluetooth paprefs pasystray pavucontrol
+sudo pacman -S --needed xdg-user-dirs xdg-user-dirs-gtk xdg-utils xorg-server-xephyr
---
+++ .arch_stuff/setup/6-terinal.sh
@@ -0,0 +1,2 @@
+sudo pacman -S --needed man bat coreutils fish fzf dialog patch ranger subversion util-linux watchman wget xsel yad
+sudo pacman -S --needed iputils iproute2 netctl networkmanager-openvpn openvpn wpa_supplicant
