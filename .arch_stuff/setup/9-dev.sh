fbdb71ca61bdb057c8e31537de88c0278ab01d92
Кирилл Фунтов <funtovkd@eruditor-group.com>
2020-05-04 17:25:20 +0300

    stuff

---
+++ .arch_stuff/setup/9-dev.sh
@@ -0,0 +1,2 @@
+sudo pacman -S  --needed dbeaver php
+# xdebug phpunit mysql?
---
+++ .arch_stuff/setup/reflector.sh
@@ -0,0 +1,3 @@
+reflector --verbose --sort rate --country Russia > ~/mirrolist1
+sudo mv ~/mirrolist1 /etc/pacman.d/mirrorlist
+
