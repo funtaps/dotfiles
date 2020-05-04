60424c10d74677390e2d6c3bd69115acbd1b0e95
Кирилл Фунтов <funtovkd@eruditor-group.com>
2020-05-04 17:31:53 +0300

    mirrorlist

--- .arch_stuff/setup/reflector.sh
+++ .arch_stuff/setup/reflector.sh
@@ -1,3 +1,4 @@
-reflector --verbose --sort rate --country Russia > ~/mirrolist1
-sudo mv ~/mirrolist1 /etc/pacman.d/mirrorlist
+reflector --verbose --sort rate --country Russia > ~/mirrorlist1
+less ~/mirrorlist1
+sudo mv ~/mirrorlist1 /etc/pacman.d/mirrorlist
 
